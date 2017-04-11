0f558bcf7c3353b40c26796a78d0e6172ad52e17
ajex13 <ajex13@gmail.com>
2017-04-12 04:31:03 +0530

    remove searchkick from model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :item_categories
   has_many :categories, through: :item_categories
-  searchkick
+  #searchkick
   validates :question, :answer, presence: true
 end
